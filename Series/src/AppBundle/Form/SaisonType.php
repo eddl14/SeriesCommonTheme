f0e1384dba53a1a63f4434963895e7428578d672
Edouard Soyer <Edouard Soyer>
2016-09-15 20:20:26 +0200

    Changes

--- Series/src/AppBundle/Form/SaisonType.php
+++ Series/src/AppBundle/Form/SaisonType.php
@@ -4,6 +4,7 @@ namespace AppBundle\Form;
 
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -19,7 +20,7 @@ class SaisonType extends AbstractType
         $builder
             ->add('nomSaison',TextType::class,['label'=>'Nom'])
             ->add('numeroSaison',TextType::class,['label'=>'Numéro saison'])
-            ->add('resumeSaison',TextType::class,['label'=>'Résumé'])
+            ->add('resumeSaison',TextareaType::class,['label'=>'Résumé'])
             ->add('anneeProduction',DateType::class,['widget'=>'single_text','format'=>'yyyy-MM-dd'])
             ->add('posterSaison',TextType::class,['label'=>'Poster'])
         ;
