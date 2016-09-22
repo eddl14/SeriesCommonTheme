4d9ca19f5437754f05ec0efa3fd4ad19cce166a3
Maxence Gougeon <maxence.nebelheim@gmail.com>
2016-09-22 10:36:50 +0200

    Real

--- Series/src/AppBundle/Form/RealisateurType.php
+++ Series/src/AppBundle/Form/RealisateurType.php
@@ -5,9 +5,9 @@ namespace AppBundle\Form;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
-use Symfony\Component\Form\Test\FormBuilderInterface;
+use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
-use Symfony\Component\Form\Extension\Core\Type\UrlType;
+
 
 
 
